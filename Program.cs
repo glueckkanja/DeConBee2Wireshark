aeac4fef82450b244f7bc1f3f6ffa29db4b5ccd9
Felix Storm <felix.storm@glueckkanja.com>
2017-05-12 15:31:56 +0200

    first release

--- Program.cs
+++ Program.cs
@@ -1,118 +1,214 @@
-﻿using System;
+﻿using Mono.Options;
+using System;
 using System.IO.Ports;
 using System.Text;
 using System.Threading;
+using System.Linq;
 using Wireshark;
+using HexDump;
+using System.IO;
+using System.Diagnostics;
 
 namespace DeConBee2Wireshark
 {
     class Program
     {
+        static TextWriter TextOutput = Console.Error;
+        static bool Verbose = false;
+        static SerialPort ComPort;
+
         static void Main(string[] args)
         {
-            var wiresharkSender = new WiresharkSender("conbee", 0xc3);
-            while(!wiresharkSender.isConnected)
+            // parse command line options
+
+            string serialPortName = SerialPort.GetPortNames().FirstOrDefault();
+            string pipeName = "conbee";
+            byte zigBeeChannel = 15;
+            string wiresharkExe = "";
+
+            var options = new OptionSet();
+            options.Add(new OptionSet.Category("USAGE: DeConBee2Wireshark [options]"));
+            options.Add("p|port=", "Serial Port (e.g. 'COM3', default is first available serial port)", x => serialPortName = x);
+            options.Add("i|pipe=", String.Format(@"Pipe Name (defaults to '{0}' => '\\.\pipe\{0}')", pipeName), x => pipeName = x);
+            options.Add("c|channel=", String.Format("ZigBee Channel to Listen on (defaults to {0})", zigBeeChannel), (byte x) => zigBeeChannel = x);
+            options.Add("w|wireshark=", "Path to Wireshark.exe", x => wiresharkExe = x);
+            options.Add("v|verbose", "Show Verbose Output", x => Verbose = true);
+            options.Add("h|?|help", x => ShowHelpAndExit(options));
+            options.Add(new OptionSet.Category(""));
+            options.Add(new OptionSet.Category("IMPORTANT: You need a ConBee USB stick from dresden elektronik (www.dresden-elektronik.de) WITH BITCATCHER FIRMWARE for this tool to work!"));
+
+            try
             {
-                Console.Write("Waiting for Wireshark...   ");
+                var unprocessed = options.Parse(args);
+                if (unprocessed.Count > 0)
+                    throw new ArgumentException(string.Format("Unrecognized command line arguments {0}.",
+                        String.Join(" ", unprocessed.Select(x => String.Format("'{0}'", x)))
+                    ));
+            }
+            catch (Exception e)
+            {
+                ShowHelpAndExit(options, e.Message);
+            }
+
+
+            // connect to ConBee USB stick
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(serialPortName))
+                    throw new ApplicationException("No serial port found or provided");
+
+                ComPort = new SerialPort(serialPortName, 38400, Parity.None, 8, StopBits.One);
+                ComPort.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Error opening serial port", ex);
+            }
+
+            
+            // open pipe and wait for wireshark to connect
+
+            TextOutput.WriteLine("DeConBee2Wireshark started (port '{0}', pipe '{1}', zigbee channel {2})", serialPortName, pipeName, zigBeeChannel);
+            TextOutput.WriteLine();
+
+            var wiresharkSender = new WiresharkSender(pipeName, 0xc3);
+
+            bool wiresharkIsChild = !string.IsNullOrWhiteSpace(wiresharkExe);
+            if (wiresharkIsChild)
+            {
+                try
+                {
+                    Process.Start(wiresharkExe, String.Format(@"-i\\.\pipe\{0} -k", pipeName));
+                }
+                catch (Exception ex)
+                {
+                    throw new ApplicationException("Error starting Wireshark", ex);
+                }
+            }
+            else
+            {
+                TextOutput.WriteLine(@"Now start Wireshark with command line arguments -i\\.\pipe\{0} -k", pipeName);
+                TextOutput.WriteLine();
+            }
+
+            TextOutput.Write(@"Waiting for Wireshark to connect to pipe \\.\pipe\{0}: ", pipeName);
+            while (!wiresharkSender.isConnected)
+            {
+                if (!wiresharkIsChild)
+                    TextOutput.Write(".");
                 Thread.Sleep(500);
             }
-            Console.WriteLine();
-            Console.WriteLine("Wireshark connected.");
-            Console.WriteLine();
+            if (!wiresharkIsChild)
+                TextOutput.WriteLine();
 
-            var serial = new SerialPort("COM6", 38400, Parity.None, 8, StopBits.One);
-            serial.ReadTimeout = -1;
-            serial.Open();
-            var serialData = new byte[] { 0x2a, 0x04, 0x09, 0x0f, 0x00, 0xe3, 0x2a, 0x02, 0x0b, 0xf2 };
-            serial.Write(serialData, 0, serialData.Length);
+            TextOutput.WriteLine("Wireshark connected.");
+            TextOutput.WriteLine();
 
+            // discard any old frames
+            ComPort.DiscardOutBuffer();
+            ComPort.DiscardInBuffer();
+
+            // select channel
+            ConBeeSendFrame(new byte[] { 0x09, zigBeeChannel, 0x00 });
+
+            // start sniffing
+            try
+            {
+                ConBeeSendFrame(new byte[] { 0x0b });
+
+                ComPort.ReadTimeout = 1000;
+                var frameAck = ConBeeReadFrame();
+                if (frameAck[0] != 0x0c)
+                    throw new InvalidDataException();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(String.Format("None or invalid data received from COM port. Are you sure a dresden elektronik ConBee USB stick WITH BITCATCHER FIRMWARE is connected to port {0}?", serialPortName), ex);
+            }
+
+            // read frames and forward to wireshark
+            ComPort.ReadTimeout = SerialPort.InfiniteTimeout;
             while (true)
             {
-                int firstByte;
-                do
+                var payload = ConBeeReadFrame();
+                if (payload[0] == 0x50 && payload.Length > 9)
                 {
-                    firstByte = serial.ReadByte();
-                } while (firstByte != 0x2a);
-
-                var bytesToRead = serial.ReadByte();
-                serialData = new byte[bytesToRead + 2];
-                serialData[0] = (byte)firstByte;
-                int bytesRead = 0;
-                while (bytesRead < bytesToRead)
-                {
-                    bytesRead += serial.Read(serialData, 2 + bytesRead, bytesToRead - bytesRead);
-                }
-                serialData[1] = (byte)bytesRead;
-
-                Console.WriteLine(HexDump(serialData));
-
-                if (serialData[2] == 0x50 && serialData.Length > 12)
-                {
-                    wiresharkSender.SendToWireshark(serialData, 11, serialData.Length - 12);
+                    var result = wiresharkSender.SendToWireshark(payload, 9, payload.Length - 9);
+                    if (!result)
+                    {
+                        TextOutput.WriteLine();
+                        TextOutput.WriteLine("Pipe broken, Wireshark might have exited - exiting as well.");
+                        break;
+                    }
                 }
             }
         }
 
-        public static string HexDump(byte[] bytes, int bytesPerLine = 16)
+        public static void ShowHelpAndExit(OptionSet options, string errorMessage = null)
         {
-            if (bytes == null) return "<null>";
-            int bytesLength = bytes.Length;
+            TextOutput.WriteLine();
+            if (!string.IsNullOrWhiteSpace(errorMessage))
+                TextOutput.WriteLine("ERROR: {0}\r\n", errorMessage);
+            options.WriteOptionDescriptions(TextOutput);
 
-            char[] HexChars = "0123456789ABCDEF".ToCharArray();
+            Environment.Exit(-666);
+        }
 
-            int firstHexColumn =
-                  8                   // 8 characters for the address
-                + 3;                  // 3 spaces
+        static void ConBeeSendFrame(byte[] payload)
+        {
+            var fullFrame = new byte[payload.Length + 3];
+            fullFrame[0] = 0x2a;
+            fullFrame[1] = (byte)(payload.Length + 1);
+            Buffer.BlockCopy(payload, 0, fullFrame, 2, payload.Length);
 
-            int firstCharColumn = firstHexColumn
-                + bytesPerLine * 3       // - 2 digit for the hexadecimal value and 1 space
-                + (bytesPerLine - 1) / 8 // - 1 extra space every 8 characters from the 9th
-                + 2;                  // 2 spaces 
+            byte frameChecksum = 0xff;
+            for (var i = 1; i < fullFrame.Length - 1; i++)
+                frameChecksum -= fullFrame[i];
+            fullFrame[fullFrame.Length - 1] = frameChecksum;
 
-            int lineLength = firstCharColumn
-                + bytesPerLine           // - characters to show the ascii value
-                + Environment.NewLine.Length; // Carriage return and line feed (should normally be 2)
-
-            char[] line = (new String(' ', lineLength - Environment.NewLine.Length) + Environment.NewLine).ToCharArray();
-            int expectedLines = (bytesLength + bytesPerLine - 1) / bytesPerLine;
-            StringBuilder result = new StringBuilder(expectedLines * lineLength);
-
-            for (int i = 0; i < bytesLength; i += bytesPerLine)
+            if (Verbose)
             {
-                line[0] = HexChars[(i >> 28) & 0xF];
-                line[1] = HexChars[(i >> 24) & 0xF];
-                line[2] = HexChars[(i >> 20) & 0xF];
-                line[3] = HexChars[(i >> 16) & 0xF];
-                line[4] = HexChars[(i >> 12) & 0xF];
-                line[5] = HexChars[(i >> 8) & 0xF];
-                line[6] = HexChars[(i >> 4) & 0xF];
-                line[7] = HexChars[(i >> 0) & 0xF];
-
-                int hexColumn = firstHexColumn;
-                int charColumn = firstCharColumn;
-
-                for (int j = 0; j < bytesPerLine; j++)
-                {
-                    if (j > 0 && (j & 7) == 0) hexColumn++;
-                    if (i + j >= bytesLength)
-                    {
-                        line[hexColumn] = ' ';
-                        line[hexColumn + 1] = ' ';
-                        line[charColumn] = ' ';
-                    }
-                    else
-                    {
-                        byte b = bytes[i + j];
-                        line[hexColumn] = HexChars[(b >> 4) & 0xF];
-                        line[hexColumn + 1] = HexChars[b & 0xF];
-                        line[charColumn] = (b < 32 ? '·' : (char)b);
-                    }
-                    hexColumn += 3;
-                    charColumn++;
-                }
-                result.Append(line);
+                TextOutput.WriteLine("Out:");
+                TextOutput.WriteLine(Utils.HexDump(fullFrame));
             }
-            return result.ToString();
+
+            ComPort.Write(fullFrame, 0, fullFrame.Length);
+        }
+
+        static byte[] ConBeeReadFrame()
+        {
+            int frameDelimiter = -1;
+            while (frameDelimiter != 0x2a)
+            {
+                frameDelimiter = ComPort.ReadByte();
+            }
+            var frameLength = ComPort.ReadByte();
+
+            var fullFrame = new byte[frameLength + 2];
+            fullFrame[0] = (byte)frameDelimiter;
+            fullFrame[1] = (byte)frameLength;
+
+            int bytesRead = 0;
+            while (bytesRead < frameLength)
+            {
+                bytesRead += ComPort.Read(fullFrame, 2 + bytesRead, frameLength - bytesRead);
+            }
+
+            if (Verbose)
+            {
+                TextOutput.WriteLine("In:");
+                TextOutput.WriteLine(Utils.HexDump(fullFrame));
+            }
+            else
+            {
+                TextOutput.Write(".");
+            }
+
+            var payload = new byte[fullFrame[1] - 1];
+            Buffer.BlockCopy(fullFrame, 2, payload, 0, payload.Length);
+
+            return payload;
         }
     }
 }
